0b6deb34066acc9f9032302acadd97c8553a4473
onmax <maximogarciamtnez@gmail.com>
2025-09-24 16:46:14 +0200

    fix: properly exclude browser tests from CI

--- starters/nextjs/vitest.config.ts
+++ starters/nextjs/vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],
+    exclude: ['node_modules', 'tests/browser.skip/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
