af38704539dd9779619f585ee89b379fdad10736
onmax <maximogarciamtnez@gmail.com>
2025-09-29 14:08:22 +0200

    Fix Cloudflare worker wasm bootstrap

--- starters/cloudflare-d1/src/index.d.ts
+++ starters/cloudflare-d1/src/index.d.ts
@@ -1,7 +1,7 @@
 declare const _default: {
-    fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response>;
-};
-export default _default;
+  fetch: (request: Request, _env: Env, _ctx: ExecutionContext) => Promise<Response>
+}
+export default _default
 interface Env {
 }
-//# sourceMappingURL=index.d.ts.map
+// # sourceMappingURL=index.d.ts.map
